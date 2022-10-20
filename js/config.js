4b892d9422910ac6495238c4e8d30fb988a2554f
Joey Cadieux <instructor.joeycadieux@gmail.com>
2022-10-20 17:58:52 +0000

    inm.

--- js/config.js
+++ js/config.js
@@ -1,9 +1,4 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.12.1/firebase-app.js";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyARUk2wxPIz5R16lDD1ghPXQbmxPqhW6Qk",
   authDomain: "techflowltd.firebaseapp.com",
